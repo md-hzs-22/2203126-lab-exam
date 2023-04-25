283ae157d64111c95e2c648acd2b639ffd6705f0
Sabugar Mo Hamza <128530225+md-hzs-22@users.noreply.github.com>
2023-04-25 12:00:49 +0530

    Update 2203126_data_filter.sh

--- code/2203126_data_filter.sh
+++ code/2203126_data_filter.sh
@@ -9,7 +9,9 @@ s=${o::2} #String slicing
 
 touch [$s]_[$2]_[$3]_A9.csv #Creating a new file
 
-echo Filename = $1 , Fieldname = $2 , Field Value = $3 , number of lines found in $1 is 500, along with Header >> [$s]_[$p]_[$q]_A9.csv
+echo Filename = $1 , Fieldname = $2 , Field Value = $3 , number of entries found in $1 with matching field values: >> [$s]_[$p]_[$q]_A9.csv
+
+grep -i $3 $1 | wc -l | cat >> [$s]_[$2]_[$3]_A9.csv
 
 head -1 $o | cat >> [$s]_[$2]_[$3]_A9.csv #Adding Headers to the file
 
